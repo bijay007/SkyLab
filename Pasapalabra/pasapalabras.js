a92942e23a6f9a0352bccbcb8bd2ae9337cd374f
Bijay Timilsina <bjtimi.007@gmail.com>
2016-12-17 22:02:41 +0100

    Add files via upload
    initial files...a lot to add

---
+++ Pasapalabra/pasapalabras.js
@@ -0,0 +1,44 @@
+var allLetters = [{
+			id : 0,
+			desc : "La gente se sienta sobre él",
+			word : "silla"
+			}, 
+
+			{
+			id : 1,
+			desc : "Es una ciudad de españa.",
+			word : "barcelona"	
+			},
+
+			{
+			id : 2,	
+			desc : "Es un vehiculo.",
+			word : "coche"	
+			}
+];
+function askUser() {
+	var userWord = prompt("Adivina la palabra : ");
+	return userWord;
+}
+function checker() {
+	var playMore = confirm("Jugar ?");
+	if (playMore){
+		compare();
+	}
+}	
+function compare() {
+	var totalPoints = 0;
+	allLetters.forEach(function(elements) {
+		alert("Consejo : " + elements.desc);
+		var userWord = askUser();
+		if (userWord === elements.word) {
+			totalPoints++;
+			alert("Genial. Tienes " + totalPoints + " puntos.");
+		} else {
+			alert("La palabra era " + elements.word);
+		}
+	});
+	alert("Enhorabuena!! Has completado el desafio con " + totalPoints + " puntos.");
+	checker();
+}
+checker();
