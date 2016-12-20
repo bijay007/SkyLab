e8199bb51834e900dba4677164b0973f32c27c4e
Bijay Timilsina <bjtimi.007@gmail.com>
2016-12-20 18:35:55 +0100

    Add files via upload
    oh yeah!! the baby runs now SMOOTH !!

--- Pasapalabra/pasapalabraUI.js
+++ Pasapalabra/pasapalabraUI.js
@@ -1,12 +1,11 @@
 "use strict"
-var userConfirm = document.getElementById("submit");   // all possible modifiable DOM events
+var userSubmit = document.getElementById("submit");
 var userInput = document.getElementById("word");
 var userDesc = document.getElementById("descrip");
-var userTrue = document.getElementById("correct");
-var userFalse = document.getElementById("wrong");
+var userRightAns = document.getElementById("correct");
+var userWrongAns = document.getElementById("wrong");
 var userTotal = document.getElementById("showPoints");
 var userStart = document.getElementById("start");
-var userRestart = document.getElementById("reset");
 
 var allLetters =[{id: "a",desc: "La gente se sienta sobre la",word: "silla"}, 
                 {id: "b",desc: "Es una ciudad de españa",word: "barcelona"},
@@ -35,48 +34,57 @@ var allLetters =[{id: "a",desc: "La gente se sienta sobre la",word: "silla"},
                 {id: "y",desc: "El dia despues de anteayer",word: "ayer"},
                 {id: "z",desc: "La fruta que crece bajo-tierra",word: "zanahoria"}];
 
+var currentIndex = 0, correctCounter = 0, wrongCounter = 0, totalPoints = 0;   // initialize global variable counters
 
-userStart.addEventListener("click", AtoZ);              // when user clicks 'starts', fires the function AtoZ
-userRestart.addEventListener("click", restart);        // when user clicks 'reset', fires the function restart
+userStart.addEventListener("click", startGame);   // starts new game on 'start' button press
+function startGame(){
+  var userChoice = confirm("Empieza un juego nuevo!! Pulsa 'OK' para jugar o 'Cancel' para salir");
+  if (userChoice) {
+    userTotal.innerHTML = "A jugar !! (El resultado final se mostrara aqui)";
+    askQuestions();
+  } else {
+    alert("Siento tristeza verte ir !!");
+  }
+};
 
-function AtoZ() {
-    var goodAnswer = 0, badAnswer = 0, nthItem = 0, totalPoints;   // initialize counter for good, bad, loop-start and total points
-    userDesc.innerHTML = allLetters[0].desc;        // show description for first word to start loop
-   // infiite loop below with 'while-loop' - need to apply step by step suto-confirmation logic :((((((((((( 
-    while (nthItem < allLetters.length) {                     // conditional to iterate through all objects in the array
-      userInput.innerHTML = "";                              // clears the input spaces in DOM after every iteration
-      userDesc.innerHTML = allLetters[nthItem].desc;                 
-      if (userConfirm.clicked == true) {                  // if user submits answer -> compare it and show results
-          compare();
-          nthItem++;
-      }
-      function compare() {
-          if (userInput.value == allLetters[nthItem].word) {  // checks if user entered value is correct, wrong, pasapalabra or exit
-              goodAnswer++;
-              userTrue.innerHTML = goodAnswer;            // if correct -> adds +1 to correct counter and writes in HTML
-          } else if (userInput.value == "pasapalabra") { // if pasapalabra -> no change, just shows the same previous value
-              goodAnswer += 0;
-          } else if (userInput.value == "exit") {        // if exit -> breaks the 'forEach' loop
-              restart();
-          } else {                                     // if wrong -> adds +1 to wrong counter, shows answer and writes in HTML
-              badAnswer++;
-              userFalse.innerHTML = badAnswer;
-              if (allLetters[nthItem].word.startsWith(allLetters[nthItem].id)) {
-                  userDesc.innerHTML = "Starts with the letter " + allLetters[nthItem].id + " :" + allLetters[nthItem].word;
-              } else {
-                  userDesc.innerHTML = "Contains the letter " + allLetters[nthItem].id + " : " + allLetters[nthItem].word;
-              }
-          }
-      }
-    };      
-    totalPoints = correct - wrong;                          // shows the average points scored at the end and writes in HTML
-    userTotal.innerHTML = "Congratulation!! You scored " + totalPoints + " points.";
+function askQuestions() {                           // displays description of the array objects in every iteration
+  if (currentIndex < allLetters.length) {
+    var currentQuestion = allLetters[currentIndex];
+    userDesc.innerHTML = currentQuestion.desc;
+    userInput.value = "";
+  } else {
+    totalPoints = correctCounter - wrongCounter;
+    userTotal.innerHTML = "Enhorabueno!! Has conseguido " + totalPoints + " puntos.";
+  }
+}
+
+userSubmit.addEventListener("click", submitPressed);    // executes when user enters his/her word and presses 'submit'
+function submitPressed() {
+  if (userInput.value == allLetters[currentIndex].word) {
+      correctCounter++;
+      userRightAns.innerHTML = correctCounter;
+  } else if (userInput.value == "pasapalabra") {
+      correctCounter += 0;
+  } else if (userInput.value == "exit") {
+      restart();
+  } else if (isNaN(userInput.value) == false) {
+      alert("No es un juego de PasaNúmeros. Introduzca solo palabras porfavor !!");
+      wrongCounter++;
+      userWrongAns.innerHTML = wrongCounter;
+  } else {
+      wrongCounter++;
+      userWrongAns.innerHTML = wrongCounter;
+      /*if (allLetters[currentIndex].word.startsWith(allLetters[currentIndex].id)) {
+          userDesc.innerHTML = "Empieza con la letra " + allLetters[currentIndex].id + " :" + allLetters[currentIndex].word;
+      } else {
+          userDesc.innerHTML = "Contiene la letra " + allLetters[currentIndex].id + " : " + allLetters[currentIndex].word;
+      } */
+  }
+  currentIndex++;               // increase the counter for array iterator and goes to ask new question
+  askQuestions();
 }
 
 function restart() {
-    var resetChoice = alert("This will reset your progress!! Press 'OK' to reset or 'Cancel' to continue");
-    if (resetChoice) {
-        userTotal.innerHTML = "";                           // clearing the final results of earlier game
-        AtoZ();
-    }
+  totalPoints = correctCounter - wrongCounter;
+  userTotal.innerHTML = "No era una decision buena. Has conseguido solo " + totalPoints + " puntos.";
 }
